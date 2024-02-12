e6df6f970a79111e7b555e74b0abab48609fcb97
mosayeh99 <mohamedelsayeh66@gmail.com>
2024-02-13 00:14:17 +0200

    add RouterModule to buttonLink component

--- src/app/shared/components/button-link/button-link.component.ts
+++ src/app/shared/components/button-link/button-link.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from "@angular/common";
+import {RouterModule} from "@angular/router";
 
 @Component({
   selector: 'app-button-link',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './button-link.component.html',
   styleUrl: './button-link.component.scss'
 })
