6667eea7ceae91d6d72ef90fe07b343f1ecf6585
mosayeh99 <mohamedelsayeh66@gmail.com>
2024-02-13 00:29:53 +0200

    create footer component

---
+++ src/app/components/layout/footer/footer.component.ts
@@ -0,0 +1,43 @@
+import {Component} from '@angular/core';
+import {ButtonIconComponent} from "../../../shared/components/button-icon/button-icon.component";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {ButtonLinkComponent} from "../../../shared/components/button-link/button-link.component";
+import {CommonModule} from "@angular/common";
+import {RouterModule} from "@angular/router";
+import {IconLinkedinComponent} from "../../../shared/icons/linkedin/icon-linkedin.component";
+import {IconYoutubeComponent} from "../../../shared/icons/youtube/icon-youtube.component";
+import {IconInstagramComponent} from "../../../shared/icons/instagram/icon-instagram.component";
+import {IconFacebookComponent} from "../../../shared/icons/facebook/icon-facebook.component";
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    ButtonIconComponent,
+    ButtonLinkComponent,
+    ReactiveFormsModule,
+    IconLinkedinComponent,
+    IconYoutubeComponent,
+    IconInstagramComponent,
+    IconFacebookComponent
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+  constructor(private fb: FormBuilder) {
+  }
+
+  openExploreAccordion: Boolean = false;
+  openProjectsAccordion: Boolean = false;
+
+  newsLetterForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]]
+  });
+
+  onSubmit(formValue: any) {
+    console.log(formValue);
+  }
+}
