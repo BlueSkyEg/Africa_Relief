d2509a32caf5f69e69a5c05b9e16dba3cc400f54
mosayeh99 <mohamedelsayeh66@gmail.com>
2024-02-12 22:38:22 +0200

    create form label components

---
+++ src/app/shared/components/form/label/label.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-label',
+  standalone: true,
+  imports: [],
+  templateUrl: './label.component.html',
+  styleUrl: './label.component.scss'
+})
+export class LabelComponent {
+
+}
