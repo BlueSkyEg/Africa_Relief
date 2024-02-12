96f4af318661b2c7b47d2b2f3a43def9b893037e
mosayeh99 <mohamedelsayeh66@gmail.com>
2024-02-13 00:30:56 +0200

    create website layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {CommonModule} from "@angular/common";
+import {HeaderComponent} from "./components/layout/header/header.component";
+import {FooterComponent} from "./components/layout/footer/footer.component";
+import {SideNavComponent} from "./components/layout/side-nav/side-nav.component";
+import {SideNavService} from "./core/services/layout/side-nav.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule, MatSidenavModule, CommonModule, HeaderComponent, FooterComponent, SideNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Africa_Relief';
+export class AppComponent implements OnInit {
+  opened: boolean = false;
+
+  constructor(private sideNavService: SideNavService) {
+  }
+
+  ngOnInit(): void {
+    this.sideNavService.sideNav().subscribe({
+      next: value => this.opened = value
+    })
+  }
 }
