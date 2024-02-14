5b0fc235e6a177000414db723f7aa030fc3c17e2
mosayeh99 <mohamedelsayeh66@gmail.com>
2024-02-14 12:56:50 +0200

    update Home main slider

--- src/app/components/home/main-slider/main-slider.component.ts
+++ src/app/components/home/main-slider/main-slider.component.ts
@@ -1,11 +1,12 @@
-import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import KeenSlider, { KeenSliderInstance } from "keen-slider"
+import {Component, OnInit, CUSTOM_ELEMENTS_SCHEMA, signal} from '@angular/core';
 import {IMainSliderSlide} from "../../../core/interfaces/home/main-slider-slide-interface";
 import {HomeService} from "../../../core/services/home/home.service";
 import {ButtonLinkComponent} from "../../../shared/components/button-link/button-link.component";
 import {IconArrowLeftComponent} from "../../../shared/icons/arrow-left/icon-arrow-left.component";
 import {IconArrowRightComponent} from "../../../shared/icons/arrow-right/icon-arrow-right.component";
 import {IconDirective} from "../../../shared/directives/icon.directive";
+import {SwiperContainer} from "swiper/swiper-element";
+import {SwiperOptions} from "swiper/types";
 
 @Component({
   selector: 'app-main-slider',
@@ -17,73 +18,46 @@ import {IconDirective} from "../../../shared/directives/icon.directive";
     IconDirective
   ],
   templateUrl: './main-slider.component.html',
-  styleUrls: [
-    '../../../../../node_modules/keen-slider/keen-slider.min.css',
-    './main-slider.component.scss'
-  ]
+  styleUrl: './main-slider.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class MainSliderComponent implements OnInit, AfterViewInit, OnDestroy {
+export class MainSliderComponent implements OnInit {
   constructor(private homeService: HomeService) {
   }
 
-  @ViewChild("sliderRef") sliderRef: ElementRef<HTMLElement>;
-
-  loaded: boolean[] = [true]
-  slider: KeenSliderInstance = null;
   slides: IMainSliderSlide[];
+  swiperElement = signal<SwiperContainer | null>(null);
 
   ngOnInit(): void {
     this.homeService.getMainSliderSlides()
       .subscribe({
-        next: (res: IMainSliderSlide[]) => this.slides = res
-      })
+        next: (res: IMainSliderSlide[]) => {
+          this.slides = res;
+
+          // Handel Slider Options
+          const swiperElementConstructor: SwiperContainer = document.querySelector('.main-slider');
+          const  swiperOptions: SwiperOptions = {
+            loop: true,
+            autoplay: {
+              delay: 3500,
+              disableOnInteraction: true
+            },
+            slidesPerView: 1,
+            navigation: {
+              enabled: true,
+              nextEl: '.main-slider-next',
+              prevEl: '.main-slider-prev',
+            },
+          };
+
+          Object.assign(swiperElementConstructor!, swiperOptions);
+          this.swiperElement.set(swiperElementConstructor as SwiperContainer);
+          this.swiperElement()?.initialize();
+        }
+      });
   }
 
-  ngAfterViewInit() {
-    this.slider = new KeenSlider(
-      this.sliderRef.nativeElement,
-      {
-        animationEnded: (s) => {
-          const idx = s.track.details.rel
-          this.loaded[idx] = true
-        },
-        loop: true,
-        initial: 0,
-      },
-      [
-        (slider) => {
-          let timeout;
-          let mouseOver: boolean = false;
-          function clearNextTimeout() {
-            clearTimeout(timeout)
-          }
-          function nextTimeout() {
-            clearTimeout(timeout)
-            if (mouseOver) return
-            timeout = setTimeout(() => {
-              slider.next()
-            }, 5000)
-          }
-          slider.on("created", () => {
-            slider.container.addEventListener("mouseover", (e) => {
-              mouseOver = true
-              clearNextTimeout()
-            })
-            slider.container.addEventListener("mouseout", () => {
-              mouseOver = false
-              nextTimeout()
-            })
-            nextTimeout()
-          })
-          slider.on("dragStarted", clearNextTimeout)
-          slider.on("animationEnded", nextTimeout)
-          slider.on("updated", nextTimeout)
-        },
-      ]
-    )
-  }
-
-  ngOnDestroy() {
-    if (this.slider) this.slider.destroy()
+  onSlideChange() {
+    console.log('changed')
   }
 }
