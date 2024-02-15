4f7885227776f4642790095f1729fbd0e0e51784
mosayeh99 <mohamedelsayeh66@gmail.com>
2024-02-15 17:10:05 +0300

    update responsive of donation card section

--- src/app/components/home/donation-card-section/donation-card-section.component.ts
+++ src/app/components/home/donation-card-section/donation-card-section.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {DonationCardComponent} from "../../../shared/components/donation-card/donation-card.component";
+import {IconZelleComponent} from "../../../shared/icons/zelle/icon-zelle.component";
 
 @Component({
   selector: 'app-donation-card-section',
   standalone: true,
   imports: [
-    DonationCardComponent
+    DonationCardComponent,
+    IconZelleComponent
   ],
   templateUrl: './donation-card-section.component.html',
   styles: ''
