b96c092233bb0a67471e778a075a3c457af004da
mosayeh99 <mohamedelsayeh66@gmail.com>
2024-02-15 17:14:54 +0300

    update donation modal

--- src/app/core/services/home/home.service.ts
+++ src/app/core/services/home/home.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {IMainSliderSlide} from "../../interfaces/home/main-slider-slide-interface";
+import {environment} from "../../../../environments/environment";
+import {IBlog} from "../../../shared/interfaces/blog-interface";
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +13,10 @@ export class HomeService {
   constructor(private http: HttpClient) { }
 
   getMainSliderSlides(): Observable<IMainSliderSlide[]> {
-    return this.http.get<IMainSliderSlide[]>('/assets/db/home-slider/home-slider.json');
+    return this.http.get<IMainSliderSlide[]>(environment.apiUrl + '/home-slider/home-slider.json');
+  }
+
+  getLatestBlogs(): Observable<IBlog[]> {
+    return this.http.get<IBlog[]>(environment.apiUrl + '/home-latest-blogs/home-latest-blogs.json');
   }
 }
