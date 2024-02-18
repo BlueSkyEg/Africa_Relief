150b06b22bc370c04eeab9e4be21f5e16101b0f9
mosayeh99 <mohamedelsayeh66@gmail.com>
2024-02-19 00:22:28 +0200

    activate category projects page

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CategoriesFilterComponent } from "../../shared/components/categories-filter/categories-filter.component";
 import { ProjectService } from '../../core/services/projects/project.service';
 import { ICategory } from '../../shared/interfaces/category-interface';
 import { IProjectCard } from '../../shared/interfaces/project-card-interface';
 import { ProjectCardComponent } from "../../shared/components/projects/project-card/project-card.component";
+import { IApiResponse } from '../../shared/interfaces/api-response-interface';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-projects',
@@ -15,16 +17,22 @@ import { ProjectCardComponent } from "../../shared/components/projects/project-c
 export class ProjectsComponent implements OnInit {
   projectCategories: ICategory[];
   projects: IProjectCard[];
-
-  constructor(private projectService: ProjectService) {}
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+  projectService: ProjectService = inject(ProjectService);
 
   ngOnInit(): void {
+    // Get project categories
     this.projectService.getProjectCategories().subscribe({
-      next: (value: ICategory[]) => this.projectCategories = value
+      next: (res: IApiResponse<ICategory[]>) => this.projectCategories = res.data
     });
 
-    this.projectService.getProjects().subscribe({
-      next: (value: IProjectCard[]) => this.projects = value
+    // Get projects
+    this.activeRoute.paramMap.subscribe({
+      next: route => {
+        this.projectService.getProjects(route.get('slug')).subscribe({
+          next: (res: IApiResponse<IProjectCard[]>) => this.projects = res.data
+        });
+      }
     });
   }
 }
--- src/app/core/services/projects/project.service.ts
+++ src/app/core/services/projects/project.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { ICategory } from '../../../shared/interfaces/category-interface';
 import { IProjectCard } from '../../../shared/interfaces/project-card-interface';
+import { IApiResponse } from '../../../shared/interfaces/api-response-interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,14 @@ export class ProjectService {
 
   constructor(private http: HttpClient) { }
 
-  getProjectCategories(): Observable<ICategory[]> {
-    return this.http.get<ICategory[]>(environment.apiUrl + '/projects/data/project-categories.json');
+  getProjectCategories(): Observable<IApiResponse<ICategory[]>> {
+    return this.http.get<IApiResponse<ICategory[]>>(environment.apiUrl + '/projects/data/project-categories.json');
   }
 
-  getProjects(): Observable<IProjectCard[]> {
-    return this.http.get<IProjectCard[]>(environment.apiUrl + '/projects/data/projects-cards.json');
+  getProjects(projectSlug: string|null): Observable<IApiResponse<IProjectCard[]>> {
+    if(!projectSlug) {
+      return this.http.get<IApiResponse<IProjectCard[]>>(environment.apiUrl + '/projects/data/projects-cards.json');
+    }
+    return this.http.get<IApiResponse<IProjectCard[]>>(environment.apiUrl + '/projects/data/category-projects-cards/' + projectSlug + '.json');
   }
 }
