9d3e7d8ceaf4bb39898565a5590731b080d3af97
mosayeh99 <mohamedelsayeh66@gmail.com>
2024-04-07 10:30:46 +0200

    handel project for production

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,7 +9,6 @@ import { RequestOptionsInterceptor } from './core/interceptors/request-options.i
 import { loaderInterceptor } from './core/interceptors/loader.interceptor';
 import { provideNgxStripe } from 'ngx-stripe';
 import { environment } from '../environments/environment';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 const scrollConfig: InMemoryScrollingOptions = {
   scrollPositionRestoration: 'top',
@@ -26,7 +25,6 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(HttpClientModule),
     provideHttpClient(withInterceptors([loaderInterceptor, RequestOptionsInterceptor])),
     provideNgxStripe(environment.stripePublicKey),
-    {provide: TitleStrategy, useClass: CustomTitleService},
-    {provide: LocationStrategy, useClass: HashLocationStrategy}
+    {provide: TitleStrategy, useClass: CustomTitleService}
   ]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://127.0.0.1:9000/api',
+  apiUrl: 'https://backend.africareliefa1.sg-host.com/api',
   stripePublicKey: 'pk_test_51KhKggKvfkJQMMfWXwnofeDI9QO7jSLGTWVv7VxW4DcYR9oMGcBYMCxbYBUfi7GPolwRLYf5VTWqmR2u9RDwYUHD00Z3MHfdWW'
 };
