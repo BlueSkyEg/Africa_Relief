9e1b86fcab068cc8b0aebc3c34f4ef03d914de8b
mosayeh99 <mohamedelsayeh66@gmail.com>
2024-04-16 11:03:37 +0200

    create video modal in home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import {MainSliderComponent} from "./main-slider/main-slider.component";
-import {DonationCardSectionComponent} from "./donation-card-section/donation-card-section.component";
 import {ProjectCategoriesSliderComponent} from "../../shared/components/projects/project-categories-slider/project-categories-slider.component";
 import {ProjectsSliderSectionComponent} from "./projects-slider-section/projects-slider-section.component";
 import {PartnersSliderComponent} from "./partners-slider/partners-slider.component";
-import {DonateByZelleComponent} from "./donate-by-zelle/donate-by-zelle.component";
 import {SafeAndEasyDonationComponent} from "../../shared/components/safe-and-easy-donation/safe-and-easy-donation.component";
 import {LatestBlogsComponent} from "./latest-blogs/latest-blogs.component";
 import { ButtonLinkComponent } from "../../shared/components/button-link/button-link.component";
@@ -14,19 +12,30 @@ import { IconZelleComponent } from "../../shared/icons/zelle/icon-zelle.componen
 import { IDonationForm } from '../../shared/interfaces/donation/donation-form.interface';
 import { IconEnvelopeComponent } from "../../shared/icons/envelope/icon-envelope.component";
 import { IconPinComponent } from "../../shared/icons/pin/icon-pin.component";
+import { animate, style, transition, trigger } from '@angular/animations';
+import { IconCloseComponent } from "../../shared/icons/close/icon-close.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styles: ``,
+    animations: [
+        trigger('overlayAnimation', [
+            transition(':enter', [
+                style({ opacity: 0 }),
+                animate('100ms', style({ opacity: 0.75 })),
+            ]),
+            transition(':leave', [
+                animate('100ms', style({ opacity: 0 }))
+            ])
+        ])
+    ],
     imports: [
         MainSliderComponent,
-        DonationCardSectionComponent,
         ProjectCategoriesSliderComponent,
         ProjectsSliderSectionComponent,
         PartnersSliderComponent,
-        DonateByZelleComponent,
         SafeAndEasyDonationComponent,
         LatestBlogsComponent,
         ButtonLinkComponent,
@@ -34,10 +43,13 @@ import { IconPinComponent } from "../../shared/icons/pin/icon-pin.component";
         DonationCardComponent,
         IconZelleComponent,
         IconEnvelopeComponent,
-        IconPinComponent
+        IconPinComponent,
+        IconCloseComponent
     ]
 })
 export class HomeComponent {
+
+  videoModalOpened: Boolean = false;
 
   donationForm: IDonationForm = {
     id: 14577,
@@ -68,5 +80,10 @@ export class HomeComponent {
 
   scroll(el: HTMLElement) {
     el.scrollIntoView({behavior: 'smooth'});
-}
+  }
+
+  toggleVideoModal() {
+    this.videoModalOpened = !this.videoModalOpened;
+    this.videoModalOpened ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'auto';
+  }
 }
