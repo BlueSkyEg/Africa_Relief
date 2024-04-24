c5ff467b60e5ee1853330d84698d51caf0cbd4fc
mosayeh99 <mohamedelsayeh66@gmail.com>
2024-04-24 15:31:02 +0200

    Add profile menu to header

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,23 +1,46 @@
-import { Component, OnInit, inject } from '@angular/core';
-import {RouterModule} from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule} from "@angular/router";
 import {LayoutService} from "../../../core/services/layout/layout.service";
 import {ButtonLinkComponent} from "../../../shared/components/button-link/button-link.component";
 import { AuthService } from '../../../core/services/auth/auth.service';
+import { IUser } from '../../../shared/interfaces/auth/user.interface';
+import {MatMenuModule} from '@angular/material/menu';
+import { IApiResponse } from '../../../shared/interfaces/api-response-interface';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, ButtonLinkComponent],
+  imports: [RouterModule, ButtonLinkComponent, MatMenuModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styles: ``
 })
 export class HeaderComponent {
 
+  isUserAuthed: boolean = false;
+
+  router: Router = inject(Router);
   layoutService: LayoutService = inject(LayoutService);
   authService: AuthService = inject(AuthService);
-  isUserAuthed: boolean = this.authService.isUserAuthed();
+
+  constructor() {
+    this.authService.authedUserSubject.asObservable().subscribe({
+      next: (user: IUser) => user ? this.isUserAuthed = true : this.isUserAuthed = false
+    })
+  }
 
   openSideNav(): void {
     this.layoutService.sideNavSubject.next(true);
+  }
+
+  onLogout() {
+    this.authService.logout().subscribe({
+      next: (res: IApiResponse<null>) => {
+        if(res.success) {
+          localStorage.clear();
+          this.authService.authedUserSubject.next(null);
+          this.router.navigateByUrl('/login');
+        }
+      }
+    })
   }
 }
