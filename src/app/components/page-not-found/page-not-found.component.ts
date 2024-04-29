76d3bfab2d3f5421832571d032017ddf36788c5d
mosayeh99 <mohamedelsayeh66@gmail.com>
2024-04-29 12:38:47 +0300

    create 404 page

---
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonLinkComponent } from "../../shared/components/button-link/button-link.component";
+
+@Component({
+    selector: 'app-page-not-found',
+    standalone: true,
+    templateUrl: './page-not-found.component.html',
+    styles: ``,
+    imports: [ButtonLinkComponent]
+})
+export class PageNotFoundComponent {
+
+}
