18978e475ecc726c9e8a8f4d25a9b134e9843a30
mosayeh99 <mohamedelsayeh66@gmail.com>
2024-05-20 17:48:02 +0300

    handel blogs gallery endpoint

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -5,6 +5,8 @@ import { IGalleryImage } from '../../shared/interfaces/blog/gallery-image.interf
 import { RouterModule } from '@angular/router';
 import { BreadcrumbComponent } from "../../shared/components/breadcrumb/breadcrumb.component";
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { IPaginatedData } from '../../shared/interfaces/paginated-data.interface';
 
 @Component({
     selector: 'app-gallery',
@@ -16,19 +18,36 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 export class GalleryComponent implements OnInit {
   gallery: IGalleryImage[] = [];
   paginationPageNum: number = 1;
+  paginationPerPage: number;
+  isPaginationLastPage: boolean = false;
 
   blogService: BlogService = inject(BlogService);
+  breakpointObserver: BreakpointObserver = inject(BreakpointObserver);
+
+  constructor() {
+    // Determine pagination perPage number based on screen size
+    this.breakpointObserver
+      .observe('(min-width: 800px)')
+      .subscribe({
+        next: (value) => this.paginationPerPage = value.matches ? 9 : 5
+      })
+  }
 
   ngOnInit(): void {
     this.onGetGallery();
   }
 
   onGetGallery() {
-    this.blogService.getGalleryImages(this.paginationPageNum).subscribe({
-      next: (res: IApiResponse<IGalleryImage[]>) => {
-        this.gallery.push(...res.data);
-        this.paginationPageNum++;
-      }
-    });
+    if(!this.isPaginationLastPage) {
+      this.blogService.getBlogsGallery(this.paginationPageNum, this.paginationPerPage).subscribe({
+        next: (res: IApiResponse<IPaginatedData<IGalleryImage[]>>) => {
+          this.gallery.push(...res.data.data);
+          if(res.data.pagination.current_page === res.data.pagination.last_page) {
+            this.isPaginationLastPage = true;
+          }
+          this.paginationPageNum++;
+        }
+      });
+    }
   }
 }
