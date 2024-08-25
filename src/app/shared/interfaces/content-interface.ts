1a81d1ae1651a8f892dfc588b55c31889137a76c
Marwa Mahmoud <mm3764453@gmail.com>
2024-08-25 13:30:58 +0300

    edit single-blog , single-project , single-career

--- src/app/shared/interfaces/content-interface.ts
+++ src/app/shared/interfaces/content-interface.ts
@@ -1,6 +1,5 @@
-import { IImage } from "./image-interface";
 export interface IContent {
-  heading: string,
-  body: string|string[]|IImage;
-  order?:number;
+  type: 'heading' | 'paragraph' | 'list';
+  body: string | string[] ;
+  order?: number;
 }
