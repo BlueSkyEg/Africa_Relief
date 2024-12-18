9a7107bd9607c123740ae6d45684159a6ce52506
Marwa Mahmoud <marwa@bluskyint.com>
2024-12-18 13:05:20 +0200

    Converted tables in donation history and subscriptions to card layouts for better mobile responsiveness'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
       screens: {
-        '2xl': '1248px'
-      }
+        "2xl": "1248px",
+      },
     },
     colors: {
-      'primary': '#205926',
-      'secondary': '#FFD500',
-      'gray-dark': '#403F3E',
-      'gray-medium': '#CCCCCC',
-      'gray-light': '#DEDEDE',
-      'desert': '#F7F6F5',
-      'black': '#1F1F1F',
-      'white': '#FFFFFF',
-      'success': '#42A162',
-      'danger': '#BA4444',
-      'warning': '#C28930',
-      'safe': '#307CC2'
+      primary: "#205926",
+      secondary: "#FFD500",
+      "gray-dark": "#403F3E",
+      "gray-medium": "#CCCCCC",
+      "gray-light": "#DEDEDE",
+      desert: "#F7F6F5",
+      black: "#1F1F1F",
+      white: "#FFFFFF",
+      success: "#42A162",
+      danger: "#BA4444",
+      warning: "#C28930",
+      safe: "#307CC2",
     },
     fontFamily: {
-      'inter': ['Inter', 'sans-serif'],
-      'roboto': ['Roboto Serif', 'sans-serif'],
+      inter: ["Inter", "sans-serif"],
+      roboto: ["Roboto Serif", "sans-serif"],
     },
     extend: {},
   },
   plugins: [],
-}
+};
 
